ffc9285f95fa3ebc6bcc7aec9c0a98f63d8a0a41
ibrahimahmed2008 <ibrahimahmed43543@gmail.com>
2026-04-15 01:19:58 +0500

    First commit

---
+++ app/src/components/layouts/Navbar.tsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div>
+          <span>LOGO</span>
+        </div>
+        <div>
+          <div>
+            <span>Home</span> <span>Shop</span> <span>Contact</span>
+          </div>
+        </div>
+        <div>
+          <button>Cart</button>
+        </div>
+      </nav>
+    </div>
+  );
+}
---
+++ app/src/components/layouts/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+        <div>
+            Contact me at 123456789
+        </div>
+    );
+}
